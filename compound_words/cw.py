bd402435566e5bcac2233c6eda0894b8d738ceac
freerangehen <email2henryip@gmail.com>
2022-07-20 00:13:38 +0100

    common first/last functions

--- compound_words/cw.py
+++ compound_words/cw.py
@@ -1,5 +1,6 @@
 import argparse
 from collections import defaultdict
+import random
 from typing import List
 
 import pandas as pd
@@ -23,7 +24,10 @@ def _get_args() -> argparse.Namespace:
 
 class CompoundWordDB:
 
-    def __init__(self, src_file):
+    def __init__(self, src_file: str, last_th: int=5, first_th=5):
+        self._first_th = first_th
+        self._last_th = last_th
+
         _df = pd.read_csv(src_file)
         
         self._first_dict = defaultdict(set)
@@ -32,6 +36,17 @@ class CompoundWordDB:
         for _first, _last in _df[["c1", "c2"]].values:
             self._first_dict[_first].add(_last)
             self._last_dict[_last].add(_first)
+
+        # if number of prefix >= threshold, place in first_dict_th
+        self._first_dict_th = dict()
+        for prefix, suffix_list in self._first_dict.items():
+            if len(suffix_list) >= first_th:
+                self._first_dict_th.update({prefix: suffix_list})
+
+        self._last_dict_th = dict()
+        for suffix, prefix_list in self._last_dict.items():
+            if len(prefix_list) >= last_th:
+                self._last_dict_th.update({suffix: prefix_list})
 
     def common_start(self, word: str) -> List[str]:
         """
@@ -46,6 +61,24 @@ class CompoundWordDB:
         """
         cl = self._last_dict.get(word, [])
         return [f"{item}{word}" for item in cl]
+
+    def common_last_quiz(self):
+        """
+        Returns a random word list, all can form respective compound words by
+        having a common last word atached to them respectively
+        """
+        key = random.choice(list(self._last_dict_th.keys()))
+        options = random.choices(list(self._last_dict_th[key]), k=self._last_th)
+        return key, options
+
+    def common_first_quiz(self):
+        """
+        Returns a random word list, all can form respective compound words by
+        having a common first word attached to them respectively.
+        """
+        key = random.choice(list(self._first_dict_th.keys()))
+        options = random.choices(list(self._first_dict_th[key]), k=self._first_th)
+        return key, options
 
 
 if __name__ == "__main__":
